6610a34f333e9d23c10d5b9169823e28b12285f1
Elison <elisonsanchez15@gmail.com>
2020-02-05 13:37:35 -0600

    se agrego get

--- src/routes/Interes/interes.js
+++ src/routes/Interes/interes.js
@@ -5,5 +5,5 @@ route.post("/interes",interes.calculoInteres)
 route.post("/tasa_interes",interes.calculoTasaInteres)
 route.post("/tiempo",interes.calculoTiempo)
 route.post("/capital",interes.calculoCapital)
-route.get("/home",interes.getHome)
+route.get("/",interes.getHome)
 module.exports = route;
