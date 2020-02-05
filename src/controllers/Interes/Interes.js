d756a4756e0dae706cfd68c2c12cb7d52b571737
Elison <elisonsanchez15@gmail.com>
2020-02-05 08:23:14 -0600

    se crearon funciones para calculos

--- src/controllers/Interes/Interes.js
+++ src/controllers/Interes/Interes.js
@@ -7,5 +7,32 @@ module.exports = {
     } else {
       res.status(400).json("Faltan datos importantes");
     }
-  }
+  },
+  calculoTasaInteres: async (req,res)=>{
+    const { n, I, c } = req.body;
+    if (n && I && c) {
+      let i = I/(c * n);
+      res.status(200).json(i);
+    } else {
+      res.status(400).json("Faltan datos importantes");
+    }
+  },
+  calculoTiempo: async (req,res)=>{
+    const { i, I, c } = req.body;
+    if (i && I && c) {
+      let n = I/(c * i);
+      res.status(200).json(n);
+    } else {
+      res.status(400).json("Faltan datos importantes");
+    }
+  },
+  calculoCapital: async (req,res)=>{
+    const { i, I, n } = req.body;
+    if (i && I && n) {
+      let c = I/(c * i);
+      res.status(200).json(c);
+    } else {
+      res.status(400).json("Faltan datos importantes");
+    }
+  },
 };
--- src/routes/Interes/interes.js
+++ src/routes/Interes/interes.js
@@ -2,4 +2,7 @@ const express = require('express');
 const route = express.Router();
 const interes = require("../../controllers/Interes/Interes")
 route.post("/interes",interes.calculoInteres)
+route.post("/tasa_interes",interes.calculoTasaInteres)
+route.post("/tiempo",interes.calculoTiempo)
+route.post("/capital",interes.calculoCapital)
 module.exports = route;
